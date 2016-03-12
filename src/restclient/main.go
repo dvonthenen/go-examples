7f3bc4596a6bd0d2b3e374782e54e4bec63636ac
dvonthenen <david.vonthenen@emc.com>
2016-03-12 08:31:22 -0800

    Update client... most of the UI is functional. Just need the remaining REST calls

--- src/restclient/main.go
+++ src/restclient/main.go
@@ -21,14 +21,39 @@ type account struct {
 
 type accounts []account
 
-type myButtonHandler struct {
+type myBtnDelete struct {
+	id     int
+	parent gwu.Panel
+	panel  gwu.Panel
+}
+
+func (h *myBtnDelete) HandleEvent(e gwu.Event) {
+	if _, isButton := e.Src().(gwu.Button); isButton {
+
+		fmt.Println("Delete called")
+		fmt.Println("Id:", h.id)
+		fmt.Println("Parent:", h.parent.Id().String())
+		fmt.Println("Panel:", h.panel.Id().String())
+
+		//TODO Call Add REST API
+
+		h.parent.Remove(h.panel)
+
+		e.MarkDirty(h.parent)
+	}
+}
+
+type myBtnAdd struct {
 	usernames []gwu.TextBox
 	names     []gwu.TextBox
 	emails    []gwu.TextBox
 }
 
-func (h *myButtonHandler) HandleEvent(e gwu.Event) {
-	if b, isButton := e.Src().(gwu.Button); isButton {
+func (h *myBtnAdd) HandleEvent(e gwu.Event) {
+	if _, isButton := e.Src().(gwu.Button); isButton {
+
+		fmt.Println("Add called")
+
 		for i := 0; i < 3; i++ {
 			fmt.Println("Username: " + h.usernames[i].Text())
 			fmt.Println("Name: " + h.names[i].Text())
@@ -41,22 +66,112 @@ func (h *myButtonHandler) HandleEvent(e gwu.Event) {
 			}
 
 			//TODO Call Add REST API
+
+			h.usernames[i].SetText("")
+			h.names[i].SetText("")
+			h.emails[i].SetText("")
+
+			e.MarkDirty(h.usernames[i])
+			e.MarkDirty(h.names[i])
+			e.MarkDirty(h.emails[i])
 		}
-		e.MarkDirty(b)
+	}
+}
+
+func refresh(address string, port int, parent gwu.Panel, panels []gwu.Panel) {
+	for i := 0; i < len(panels); i++ {
+		parent.Remove(panels[i])
+	}
+
+	url := "http://" + address + ":" + strconv.Itoa(port) + "/user"
+	fmt.Println("URL:>", url)
+
+	req, err := http.NewRequest("GET", url, nil)
+	//req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := resp.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	fmt.Println("response Body:", string(body))
+
+	var accts accounts
+	err = json.Unmarshal(body, &accts)
+
+	for i := 0; i < len(accts); i++ {
+		fmt.Println("Id:", accts[i].Id)
+		fmt.Println("Username:", accts[i].Username)
+		fmt.Println("Name:", accts[i].Name)
+		fmt.Println("Email:", accts[i].Email)
+
+		p := gwu.NewHorizontalPanel()
+		p.SetCellPadding(2)
+		p.Add(gwu.NewLabel("Username: " + accts[i].Username))
+		p.Add(gwu.NewLabel("Name: " + accts[i].Name))
+		p.Add(gwu.NewLabel("Email: " + accts[i].Email))
+
+		btndelete := gwu.NewButton("Delete")
+		btndelete.SetAttr("align", "center")
+		btndelete.AddEHandler(&myBtnDelete{accts[i].Id, parent, p}, gwu.ETypeClick)
+		p.Add(btndelete)
+
+		fmt.Println("Panel Id:", p.Id().String())
+
+		panels = append(panels, p)
+
+		parent.Add(p)
+	}
+}
+
+type myBtnRefresh struct {
+	address string
+	port    int
+	parent  gwu.Panel
+	panels  []gwu.Panel
+}
+
+func (h *myBtnRefresh) HandleEvent(e gwu.Event) {
+	if _, isButton := e.Src().(gwu.Button); isButton {
+
+		fmt.Println("Refresh called")
+		fmt.Println("address:", h.address)
+		fmt.Println("port:", h.port)
+		fmt.Println("parent:", h.parent.Id().String())
+		for i := 0; i < len(h.panels); i++ {
+			fmt.Println("panel:", h.panels[i].Id().String())
+		}
+
+		refresh(h.address, h.port, h.parent, h.panels)
+
+		e.MarkDirty(h.parent)
 	}
 }
 
 func main() {
 	//define flags
 	var port int
-	flag.IntVar(&port, "port", 9050, "the REST server in which to bind to")
+	flag.IntVar(&port, "port", 9000, "the REST server in which to bind to")
 	var address string
 	flag.StringVar(&address, "address", "127.0.0.1", "the REST server in which to bind to")
 	//parse
 	flag.Parse()
 
 	// Create and build a window
-	win := gwu.NewWindow("main", "3-Tier App Demo")
+	win := gwu.NewWindow("", "3-Tier App Demo")
 	win.Style().SetFullWidth()
 	win.SetHAlign(gwu.HACenter)
 	win.SetCellPadding(2)
@@ -94,7 +209,7 @@ func main() {
 
 	btnadd := gwu.NewButton("Add")
 	btnadd.SetAttr("align", "center")
-	btnadd.AddEHandler(&myButtonHandler{usernames, names, emails}, gwu.ETypeClick)
+	btnadd.AddEHandler(&myBtnAdd{usernames, names, emails}, gwu.ETypeClick)
 	addform.Add(btnadd)
 
 	win.Add(addform)
@@ -105,62 +220,20 @@ func main() {
 	acctlist.SetCellPadding(10)
 	acctlist.SetAttr("width", "400")
 
-	p := gwu.NewHorizontalPanel()
-	p.SetCellPadding(2)
-	p.Add(gwu.NewLabel("Current List of Accounts"))
+	var panels []gwu.Panel
+
+	title2 := gwu.NewHorizontalPanel()
+	title2.SetCellPadding(2)
+	title2.Add(gwu.NewLabel("Current List of Accounts"))
 	btnrefresh := gwu.NewButton("Refresh")
 	btnrefresh.SetAttr("align", "center")
-	//btnrefresh.AddEHandler(&myButtonHandler{usernames, names, emails}, gwu.ETypeClick)
-	p.Add(btnrefresh)
+	btnrefresh.AddEHandler(&myBtnRefresh{address, port, acctlist, panels}, gwu.ETypeClick)
+	title2.Add(btnrefresh)
 
-	acctlist.Add(p)
+	acctlist.Add(title2)
 
-	//TODO: REST API call to GET... loop adding labels
-	url := "http://" + address + ":" + strconv.Itoa(port) + "/user"
-	fmt.Println("URL:>", url)
-
-	req, err := http.NewRequest("GET", url, nil)
-	//req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	bodystr, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(bodystr))
-
-	body, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1048576))
-	if err != nil {
-		panic(err)
-	}
-	if err := resp.Body.Close(); err != nil {
-		panic(err)
-	}
-
-	var accts []account
-	err = json.Unmarshal(body, &accts)
-	/*
-		for acct := range accts {
-			fmt.Printf("Id: %v", acct.Id)
-			fmt.Println("Username: %s", acct.Username)
-			fmt.Println("Name: %s", acct.Name)
-			fmt.Println("Email: %s", acct.Email)
-		}
-	*/
-	//*
-	for i := 0; i < len(accts); i++ {
-		fmt.Printf("Id: %v", accts[i].Id)
-		fmt.Println("Username: %s", accts[i].Username)
-		fmt.Println("Name: %s", accts[i].Name)
-		fmt.Println("Email: %s", accts[i].Email)
-	}
-	//	*/
+	//get current list of accounts
+	refresh(address, port, acctlist, panels)
 
 	win.Add(acctlist)
 
