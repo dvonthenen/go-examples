7069ce374e78001be5dc028dcd5918b082eb136c
dvonthenen <david.vonthenen@emc.com>
2016-03-28 14:00:42 -0700

    Update the project to have the webserver write data

--- src/statichttpserver/main.go
+++ src/statichttpserver/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
 
 func main() {
 	//define flags
@@ -17,6 +30,42 @@ func main() {
 
 	fmt.Println("path = ", path)
 	fmt.Println("port = ", port)
+
+	filename := path + "/mydata.txt"
+
+	go func() {
+		count := 0
+
+		inFile, err := os.Open(filename)
+		if err == nil {
+			defer inFile.Close()
+
+			scanner := bufio.NewScanner(inFile)
+			scanner.Split(bufio.ScanLines)
+
+			for scanner.Scan() {
+				count++
+			}
+		}
+
+		count++ //so we can start with 1 and continue where we left off
+
+		for {
+			outFile, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+			failOnError(err, "Failed to open file for append")
+			defer outFile.Close()
+
+			_, err = outFile.WriteString("Creting my data #" + strconv.Itoa(count) + "\n")
+			failOnError(err, "Failed to append file")
+
+			rand.Seed(time.Now().UnixNano())
+			random := rand.Intn(2) + 1
+			time.Sleep(time.Duration(random) * time.Second)
+
+			count++
+		}
+
+	}()
 
 	panic(http.ListenAndServe(fmt.Sprintf(":%v", port), http.FileServer(http.Dir(path))))
 }
