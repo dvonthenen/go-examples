87c0a259c8001515c68ba435490e29f4ec6f3ed3
dvonthenen <david.vonthenen@emc.com>
2016-05-31 08:44:01 -0700

    Sample code for auto discovering IPs

---
+++ src/ips/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Print("Error 1: ", err, "\n")
+		return
+	}
+	// handle err
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			fmt.Print("Error 2: ", err, "\n")
+		}
+		// handle err
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip := v.IP
+				fmt.Print("IPNet: ", ip, "\n")
+			case *net.IPAddr:
+				ip := v.IP
+				fmt.Print("IPAddr: ", ip, "\n")
+			}
+		}
+	}
+}
